743f2bd9c9b4f6ef0c3f5faf021ce502e04002dd
clawfinger <clawfinger@yandex.ru>
2016-07-08 15:28:23 +0400

    Реализован таймер и попап в трее

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,8 +6,13 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //timer = new QTimer(this);
     showTrayIcon();
     setTrayMenuActions();
+    //connect(timer, SIGNAL(timeout()), this, SLOT(timerFinished()));
+    connect(ui->buttonBox, SIGNAL(accepted()), this, SLOT(startTimer()));
+    connect(ui->buttonBox, SIGNAL(rejected()), this, SLOT(hide()));
+
 }
 
 MainWindow::~MainWindow()
@@ -18,7 +23,9 @@ void MainWindow::showTrayIcon(){
     trayIcon = new QSystemTrayIcon(this);
     QIcon trayImage(":/images/tomato.ico");
     trayIcon -> setIcon(trayImage);
-    //trayIcon -> setContextMenu(trayIconMenu);
+    trayIcon->setToolTip("Get back to work!");
+    connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
+            this, SLOT(trayIconActivated(QSystemTrayIcon::ActivationReason)));
     trayIcon -> show();
 }
 
@@ -35,4 +42,34 @@ void MainWindow::setTrayMenuActions(){
     trayIconMenu->addAction(restoreAction);
     trayIconMenu->addAction(quitAction);
     trayIcon->setContextMenu(trayIconMenu);
+}
+
+void MainWindow::changeEvent(QEvent *event){
+    if(event->type() == QEvent::WindowStateChange)
+    {
+        event->ignore();
+        this->hide();
+    }
+}
+
+void MainWindow::closeEvent(QCloseEvent *event){
+    this->hide();
+    event->ignore();
+}
+
+void MainWindow::trayIconActivated(QSystemTrayIcon::ActivationReason reason){
+    if(reason == QSystemTrayIcon::DoubleClick || reason == QSystemTrayIcon::Trigger)
+        trayActionExecute();
+}
+
+void MainWindow::startTimer()
+{
+    int time = ui->workTimeInput->value();
+    QTimer::singleShot(time*1000, this, SLOT(timerFinished()));
+    this->hide();
+
+}
+void MainWindow::timerFinished()
+{
+    trayIcon->showMessage("Timeout", "Timer is finished");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include <QSystemTrayIcon>
+#include <QCloseEvent>
+#include <QTimer>
 
 
 namespace Ui {
@@ -23,10 +25,16 @@ private:
     QMenu* trayIconMenu;
     QAction* quitAction;
     QAction* restoreAction;
+    //QTimer* timer;
 private slots:
     void trayActionExecute();
     void showTrayIcon();
     void setTrayMenuActions();
+    void changeEvent(QEvent *event);
+    void closeEvent(QCloseEvent *event);
+    void trayIconActivated(QSystemTrayIcon::ActivationReason);
+    void startTimer();
+    void timerFinished();
 };
 
 #endif // MAINWINDOW_H
